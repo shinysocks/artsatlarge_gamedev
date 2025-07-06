0a908e3f64588709a106c489b5ff51f3f56ab049
Noah Dinan <noah@shinysocks.net>
2025-07-05 19:59:12 -0500

    add fish demo!

---
+++ src/fishywinner.py
@@ -0,0 +1,54 @@
+'''
+FISHY WINNER
+
+Use this as a starting point for new creations!
+
+Daisy DiCarlo 2025
+'''
+
+import pygame
+from lib.lib import BLUE, GREEN, RED, WHITE, BLACK, Animation, Sprite, Text, Direction, Position, initialize, set_background_color, update, get_mouse_pos, mouse_clicked, get_keys, quit, wait, check_collision
+
+# Pygame keys documentation here: https://www.pygame.org/docs/ref/key.html
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+TITLE = "Fishy Winner"
+
+initialize(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE) 
+
+# Create some animations
+swim = Animation("Fishy_Sprite.png", fps=7, columns=1, rows=2, number_of_frames=2)
+still = Animation("FISHYDEFS.png", fps=1, columns=1, rows=1, number_of_frames=1)
+finishline = Animation("finishline.png", fps=1, columns=1, rows=1, number_of_frames=1)
+
+# Make some sprites!
+finish= Sprite(finishline, Position(0, 0), size=(SCREEN_WIDTH, 125))
+fishy= Sprite(still, Position(200, 600), size=(150, 150))
+text = Text(Position(SCREEN_WIDTH / 4, SCREEN_HEIGHT/2), color=WHITE)
+text2 = Text(Position(SCREEN_WIDTH / 4, (SCREEN_HEIGHT/2) + 50),  color=WHITE)
+
+custom_color = (BLUE)
+set_background_color(custom_color)
+
+
+while True:
+    # Write your game logic here!
+
+
+    if fishy.clicked():
+        fishy.set_anim(swim)
+        fishy.move(Direction.UP)
+    else:
+        fishy.set_anim(still)
+        fishy.stop()
+
+
+    # if fishy hits finish line, WIN!
+    if check_collision(fishy, finish):
+        text.set_words("FISHY WINNER")
+        text2.set_words("FISHY DINNER!")
+        
+
+    # Don't remove
+    update()
